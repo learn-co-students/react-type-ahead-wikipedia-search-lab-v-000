edc50aabae8755c5f423721806947f966aa484b2
ldgarber <leah.garber88@gmail.com>
2017-05-08 12:48:54 -0400

    Done.

--- components/Autocomplete.js
+++ components/Autocomplete.js
@@ -11,12 +11,32 @@ class Autocomplete extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      results: resultStore.getState().results, 
+      query: '', 
     };
+    this.handleChange = this.handleChange.bind(this); 
+  }
+
+  componentDidMount () {
+    this.removeListener = resultStore.addListener( ({results}) => {
+      this.setState({ results }); 
+    });   
+  }
+  componentWillUnmount () {
+    this.removeListener();  
+  }
+  handleChange(ev) {
+    this.state.query = ev.target.value; 
+    if (this.state.query.length > 2) {
+      actions.search(this.state.query);  
+    }
   }
   render() {
     return (
-      <div>
+      <div className="autocomplete">
         <h2>Autocomplete</h2>
+        <SearchField onChange={this.handleChange} value={this.state.query} /> 
+        <SearchResults results={this.state.results}/>
       </div>
     );
   }
